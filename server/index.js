fac28ac0ec04af52330daca8e73757e75b39d6f3
Your Name <borsinileuro@gmail.com>
2023-07-28 10:25:11 +0200

    fase9

--- server/index.js
+++ server/index.js
@@ -20,16 +20,16 @@ app.use(
 app.post('/upload', (req, res, next) => { 
     let uploadFile = req.files.file//si riferisce al file come nome nel campo di input.
     const name = uploadFile.name 
-    uploadFile.mv(`${__dirname}/../src/immagini/${name}`, function(err) { 
+    uploadFile.mv(`${__dirname}/../client/src/immagini/${name}`, function(err) { 
         if (err) { 
         return res.status(500).send(err ) 
         } 
         return res.status(200).json({ status: 'uploaded', name }) })
     })
 
-app.use(express.static(path.resolve(__dirname, '../build')))
+app.use(express.static(path.resolve(__dirname, '../client/build')))
 
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../build', 'index.html'))})
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))})
 
 app.listen(3001,()=>console.log('listen in port 3001'))
